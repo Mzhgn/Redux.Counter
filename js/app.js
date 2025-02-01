3954efa0b30fbd0ddb3116c90d56248fee7dff89
Mozhgan <mojganshah@gmail.com>
2025-02-01 20:13:04 +0330

    counter project added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log(Redux);
+const decBtn = document.querySelector(".dec");
+const resetBtn = document.querySelector(".reset");
+const incBtn = document.querySelector(".inc");
+
+const numberElem = document.querySelector("#num");
+
+// Declare Counter Reducer
+const counterReducer = (action) => {
+  switch (action.type) {
+    case "INCREMENT": {
+    }
+    case "DECREMENT": {
+    }
+    case "SET_ZERO": {
+    }
+    default: {
+    }
+  }
+};
