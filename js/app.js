3ada92df3e84984dde7bd5dd7a71326de858c5ed
Mozhgan <mojganshah@gmail.com>
2025-02-03 09:59:42 +0330

    setting decrement increment setzero

--- js/app.js
+++ js/app.js
@@ -5,17 +5,43 @@ const incBtn = document.querySelector(".inc");
 const numberElem = document.querySelector("#num");
 
 // Declare Counter Reducer
-const counterReducer = (action) => {
-  switch (action) {
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
     case "INCREMENT": {
+      return state + 1;
     }
     case "DECREMENT": {
+      return state - 1;
     }
     case "SET_ZERO": {
+      return state - state;
     }
     default: {
+      return state;
     }
   }
 };
 
 const store = Redux.createStore(counterReducer);
+
+// Handle Evenets
+
+decBtn.addEventListener("click", () => {
+  store.dispatch({ type: "DECREMENT" });
+  let newValue = store.getState();
+  numberElem.innerHTML = newValue;
+
+  console.log(store.getState());
+});
+resetBtn.addEventListener("click", () => {
+  store.dispatch({ type: "SET_ZERO" });
+  let newValue = store.getState();
+  numberElem.innerHTML = newValue;
+});
+incBtn.addEventListener("click", () => {
+  store.dispatch({ type: "INCREMENT" });
+  let newValue = store.getState();
+  numberElem.innerHTML = newValue;
+
+  console.log(store.getState());
+});
